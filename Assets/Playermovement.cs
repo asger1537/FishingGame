452a7d670d4caee3434f08c63e1389bf89ee192b
ulriku <ulrikhlp@gmail.com>
2019-12-05 19:18:16 +0100

    #5 #2 #1 #
    Added unity project to git
    (except big files, complete project is on unity collab, this contains player and camera movement. Also contains terrain )

---
+++ Assets/Playermovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+{
+    public CharacterController controller;
+
+    float speed = 12f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            move += transform.up * -1f;
+        }
+        
+
+        controller.Move(move * speed * Time.deltaTime);
+    }
+}
